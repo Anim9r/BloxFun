07d2e491f655b6d4798a42bd8a3dbb295c663edd
Anim9r <76840476+Anim9r@users.noreply.github.com>
2025-11-04 11:02:32 +0000

    Delete alphastatus.lua

--- alphastatus.lua
+++
@@ -1,7 +0,0 @@
-if game.Players.LocalPlayer.PlayerGui.GUI.Spectate.FPSpectating then
-    local v4l = Instance.new("StringValue",game.Players.LocalPlayer.PlayerGui.GUI.Spectate)
-    v4l.Name = "FPSonSpectate"
-    v4l.Value = "unlock" --FPS
-else
-    print("Something Wrong..")
-end
